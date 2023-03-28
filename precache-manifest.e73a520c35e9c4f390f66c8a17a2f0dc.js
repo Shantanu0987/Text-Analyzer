1fab8cac075d7a999176beb653db909a4794900d
shantanu <shantanudas0108@gmail.com>
2023-03-28 23:51:12 +0530

    Updates

---
+++ precache-manifest.e73a520c35e9c4f390f66c8a17a2f0dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "208544c92b446cc78aa4",
+    "url": "/Text-Analyzer/static/css/main.461d5b98.chunk.css"
+  },
+  {
+    "revision": "208544c92b446cc78aa4",
+    "url": "/Text-Analyzer/static/js/main.208544c9.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/Text-Analyzer/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/Text-Analyzer/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "f5268d09d3194797a4aa",
+    "url": "/Text-Analyzer/static/js/runtime~main.f5268d09.js"
+  },
+  {
+    "revision": "941a8dff9c9073f724e85284a91b63e0",
+    "url": "/Text-Analyzer/index.html"
+  }
+];
